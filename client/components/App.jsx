5729852b67fbe897ffb394d3d3a1a39839f8ed42
Student <student@eda-bc-07.localdomain>
2019-12-04 18:37:28 +1300

    added api/auth

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Login from './Login'
 
 
 
@@ -8,7 +8,11 @@ class App extends React.Component{
 
   render() {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <h1>React development has begun!</h1>
+      <Login/>
+      
+    </div>
   )
 }}
 
