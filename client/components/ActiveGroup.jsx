0ae56847ce84440da213d561f75f37c3296c5094
ethan-t-obrien <ethanobrien1009@gmail.com>
2019-12-05 09:20:35 +1300

    add activegroup component

---
+++ client/components/ActiveGroup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class ActiveGroup extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state={
+      
+    }    
+  }
+
+
+
+}
+
+export default ActiveGroup
