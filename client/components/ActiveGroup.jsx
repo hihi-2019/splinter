2c436c81310bd4dd02723f981c0e2b9dae93cc2b
ethan-t-obrien <ethanobrien1009@gmail.com>
2019-12-05 09:28:45 +1300

    return statement for activegroup component

--- client/components/ActiveGroup.jsx
+++ client/components/ActiveGroup.jsx
@@ -4,13 +4,22 @@ import React from 'react'
 class ActiveGroup extends React.Component {
   constructor(props) {
     super(props) 
-    this.state={
-      
-    }    
+    this.state={}    
   }
 
-
-
+  render() {
+    return (
+      <>
+        <h1>Name of Group</h1>
+        <h3>Group Members</h3>
+          <ul>
+            <li>
+              MemberName MemberBalance $00.00 
+            </li>
+          </ul>
+      </>
+    )
+  }
 }
 
 export default ActiveGroup
