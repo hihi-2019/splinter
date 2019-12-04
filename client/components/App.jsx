3e467a7cf0f09bcfe434855a93013cd1ca15174a
Student <student@eda-bc-07.localdomain>
2019-12-04 17:50:08 +1300

    combine reducers and App to class comp

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const App = () => {
+
+
+
+
+class App extends React.Component{
+
+  render() {
   return (
     <h1>React development has begun!</h1>
   )
-}
+}}
 
 export default App
