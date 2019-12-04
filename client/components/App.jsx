5f0e96c637acf22726765be9c26afb8dc29fc178
Student <student@eda-bc-06.localdomain>
2019-12-05 11:29:24 +1300

    changes to dashboard, landing page, footer, dashboard nav components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import {HashRouter as Router, Route, Link, Redirect} from 'react-router-dom'
+import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom'
 
 import LandingPage from '../components/LandingPage'
 import ActiveGroup from '../components/ActiveGroup'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import Dashboard from '../components/Dashboard'
 
 
 
 
-class App extends React.Component{
+class App extends React.Component {
 
   render() {
-  return (
-    <Router>
-      <Nav/>
-      <div className="content">
-        <Route exact path="/" component={LandingPage}/>
-        <Route exact path="/group" component={ActiveGroup}/>
-      </div>
-      <Footer/>
-    </Router>
-  )
-}}
+    return (
+      <Router>
+        <Nav />
+        <div className="content">
+          <Dashboard/>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/group" component={ActiveGroup} />
+        </div>
+        <Footer />
+      </Router>
+    )
+  }
+}
 
 export default App
---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import DashboardNav from './DashboardNav'
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (
+            <>
+            <DashboardNav/>
+            </>
+        )
+    }
+}
+
+export default Dashboard
