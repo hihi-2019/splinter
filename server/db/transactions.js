39b8e4518f1e8971429e14a061f33d3ec17a196a
tosca-wilson <tosc.wilson@gmail.com>
2019-12-05 16:16:05 +1300

    newtransaction db function, route, and api

--- server/db/transactions.js
+++ server/db/transactions.js
@@ -1,17 +1,14 @@
 const database = require('./connection')
 
-function addTransaction (transactionData, db = database) {
-
-  return db('transaction')
-    .insert({
-          group_id: transactionData.group_id,
-          groupMember_id: transactionData.groupMember_id,
-          transaction_total: transactionData.transaction_total,
-          transaction_name: transactionData.transaction_name,
-          date: transactionData.date
-        })
+function addTransaction(transaction, db = database) {
+  return db('transactions')
+    .insert(transaction.new_transaction)
+    .then(tid => {
+      transaction.transaction_details.transaction_id = tid[0]
+      return db('transactionDetails')
+        .insert(transaction.transaction_details)
+    })
 }
-
 
 
 module.exports = {
--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,9 +1,25 @@
 const router = require('express').Router()
 const db = require('../db/transactions')
 
-router.post('/dashboard', (req,res) => {
-  db.addTransaction(req.body)
-  .then(res => res)
+router.post('/', (req, res) => {
+  let transaction = {
+    new_transaction: {
+      group_id: req.body.group_id,
+      groupMember_id: req.body.groupMember_id,
+      transaction_total: req.body.transaction_total,
+      transaction_name: req.body.transaction_name,
+      date: Date.now()
+    },
+    transaction_details: {
+      groupMember_id: req.body.groupMember_id,
+      total_contribution: req.body.total_contribution
+    }
+  }
+  db.addTransaction(transaction).then(data => {
+    console.log(data)
+    res.send(200)
+  })
 })
 
 module.exports = router
+
