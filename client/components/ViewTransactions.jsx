7cf0d78622295a3cbcd73ac9f36a595933412910
robert-cummins <robert.k.cummins@gmail.com>
2019-12-06 14:20:48 +1300

    add info to transaction table

--- client/components/ViewTransactions.jsx
+++ client/components/ViewTransactions.jsx
@@ -1,52 +1,66 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {getTransactions} from '../actions/transactions'
+import { getTransactions } from '../actions/transactions'
 
 class ViewTransactions extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
-  componentDidMount(){
-    this.props.dispatch(getTransactions(1))
-  } 
+  componentDidMount() {
+    this.props.dispatch(getTransactions(Number(this.props.activeGroup[0])))
+  }
+
+  getGroupMember = (id) => {
+    return this.props.groupMembers.map(person => {
+      if(person.groupMember_id == id){
+        return person.member_name
+      }
+    })
+  }
 
   render() {
     return (
-     <>
-     <h3>View all transactions</h3>
-     <table className="table">
-       <thead>
-         <tr>
-           <th scope='col'>Description</th>
-           <th scope='col'>Date</th>
-           <th scope='col'>Transaction Total</th>
-         </tr>
-       </thead>
-     <tbody>
-     {this.props.transactions.map(transaction => {
-       let date = new Date(transaction.date * 1000)
-       let dateString = date.toUTCString().slice(5, 22)
-       return(
-         <tr>
-      
-         <td><h5>{transaction.transaction_name}</h5></td>
-         <td>{dateString}</td>
-         <td>{transaction.transaction_total}</td>
-     </tr>)
-      })
-    }
-     </tbody>
-    </table>
-     
-     </>  
+      <>
+        <h3>View all transactions</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope='col'>Description</th>
+              <th scope='col'>Date</th>
+              <th scope='col'>Transaction Total</th>
+              <th scope='col'>Who Paid</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.transactions.filter(transaction => transaction.total_contribution > 0).map(payers => {
+              console.log(payers)
+              let date = new Date(payers.date * 1000)
+              let dateString = date.toUTCString().slice(5, 22)
+              let name = this.getGroupMember(payers.groupMember_id)
+              console.log(name)
+              return (
+                <tr>
+                  <td>{payers.transaction_name}</td>
+                  <td>{dateString}</td>
+                  <td>$ {payers.total_contribution / 100}</td>
+                  <td>{name}</td>
+                </tr>)
+              })
+            }
+          </tbody>
+        </table>
+
+      </>
     )
   }
 }
 
 const mapStateToProps = (reduxState) => {
   return {
-    transactions: reduxState.transactions
+    transactions: reduxState.transactions,
+    groupMembers: reduxState.groupMembers,
+    activeGroup: reduxState.activeGroup
   }
 }
 
