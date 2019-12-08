851c228803903264c0bb581226916d986589028f
tosca-wilson <tosc.wilson@gmail.com>
2019-12-09 10:48:40 +1300

    format css file

--- client/components/CreateGroup.jsx
+++ client/components/CreateGroup.jsx
@@ -64,7 +64,7 @@ class CreateGroup extends React.Component {
     return (
       <>
         <div className="form-content animated fadeIn">
-          <form className="form">
+          <form className="groupForm">
           <h3 className="formTitle">Create New Group</h3>
             <label>Group Name</label>
             <input className='form-control' required type='text' name='group_name' placeholder='eg. Kates Birthday' onChange={this.updateDetails}></input>
