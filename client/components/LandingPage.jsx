1ae954b2ef20e748b3bf69b9d94ba4437e6ed8f7
tosca-wilson <tosc.wilson@gmail.com>
2019-12-09 11:33:20 +1300

    further formatting to css file

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -9,9 +9,9 @@ class LandingPage extends React.Component {
   render() {
     return (
       <>
-        <div className="landingPage titleText animated fadeIn">
+        <div className="landingPage animated fadeIn">
           <img src="money.png"></img>
-          <h1 >Sick of not getting paid back?</h1>
+          <h1 className="landingPageTitle">Sick of not getting paid back?</h1>
           <h3>Sign up for Splinter to keep track of who spent what.</h3>
           <div className="">
             <Link to="/signup"><button type="button" className="landingButtons btn btn-lg custom-button">Sign Up</button></Link>
