18d02b082411b8c93b7f1769c0b1e0788064ad18
tosca-wilson <tosc.wilson@gmail.com>
2019-12-10 11:12:34 +1300

    button styling for view transactions

--- client/components/ViewTransactions.jsx
+++ client/components/ViewTransactions.jsx
@@ -80,11 +80,11 @@ class ViewTransactions extends React.Component {
                   return (
                     <>
                       <tr>
-                        <td><button className='btn custom-button btn-block' onClick={this.toggleTransaction} name={transaction.transaction_name}>{transaction.transaction_name}</button></td>
+                        <td><button className='btn transactionsButton btn-block' onClick={this.toggleTransaction} name={transaction.transaction_name}>{transaction.transaction_name}</button></td>
                         <td>{dateString}</td>
                         <td>$ {transaction.total_contribution / 100}</td>
                         <td>{name}</td>
-                        {selectedGroup.settled == 0 && <td><button onClick={this.handleDelete} id={transaction.transaction_id} className='btn btn-danger'>Delete</button></td>}
+                        {selectedGroup.settled == 0 && <td><button onClick={this.handleDelete} id={transaction.transaction_id} className='btn btn-outline-danger'>Delete</button></td>}
                       </tr>
                       {this.state.openTransactions.includes(transaction.transaction_name) &&
                         <TransactionDetails name={transaction.transaction_name} />
