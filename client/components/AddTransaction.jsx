f1596daafe24cddc058208a9b788a6936ecc9bff
robert-cummins <robert.k.cummins@gmail.com>
2019-12-10 16:56:30 +1300

    Add error message when trying to custum split without a payer

--- client/components/AddTransaction.jsx
+++ client/components/AddTransaction.jsx
@@ -13,25 +13,31 @@ class AddTransaction extends React.Component {
       error: false,
       checked: true,
       selectedPayer: [],
-      selectedPayee: []
+      errorPayer: false
     }
   }
 
   handleCheck = () => {
+    
     if(this.state.selectedPayer.length !== 0){
+      console.log('hello')
       if(this.state.checked){
         this.setState({
           checked: false,
-          group_members: this.state.selectedPayer
+          group_members: this.state.selectedPayer,
+          errorPayer: false
         })
       } else{
         this.setState({
           checked: true,
-          group_members: this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup)
+          group_members: this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup),
+          errorPayer: false
         })
       }
     } else{
-      alert("please select 'paid by'")
+      this.setState({
+        errorPayer: true
+      })
     }
     
 }
@@ -89,7 +95,6 @@ class AddTransaction extends React.Component {
 
   deleteMember = (e) => {
     if(this.state.selectedPayer[0].member_name == e.target.name){
-      console.log(this.state.selectedPayer)
       this.handleCheck()
       return this.setState({
         group_members: this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup)
@@ -106,6 +111,7 @@ class AddTransaction extends React.Component {
 
 
   render() {
+    
    let members = this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup) 
    let splitMembers = members.filter(member => !this.state.group_members.includes(member))
     return (
@@ -139,6 +145,7 @@ class AddTransaction extends React.Component {
 
                 </div>
                 <div>
+                {this.state.errorPayer == true && <p style={{ color: "red" }}>Please select a payer</p>}
                   <label className="inputLabel">Split by all members?</label>
                   <input type='checkbox' name='membersOwing' checked={this.state.checked} onChange={this.handleCheck}></input>
                   {!this.state.checked &&
