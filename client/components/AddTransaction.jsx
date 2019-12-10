765bb2ed606b898811ba81f30349c5ac23bb96ae
tosca-wilson <tosc.wilson@gmail.com>
2019-12-10 16:22:33 +1300

    formatting to select members transaction form

--- client/components/AddTransaction.jsx
+++ client/components/AddTransaction.jsx
@@ -18,25 +18,25 @@ class AddTransaction extends React.Component {
   }
 
   handleCheck = () => {
-    if(this.state.selectedPayer.length !== 0){
-      if(this.state.checked){
+    if (this.state.selectedPayer.length !== 0) {
+      if (this.state.checked) {
         this.setState({
           checked: false,
           group_members: this.state.selectedPayer
         })
-      } else{
+      } else {
         this.setState({
           checked: true,
           group_members: this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup)
         })
       }
-    } else{
+    } else {
       alert("please select 'paid by'")
     }
-    
-}
+
+  }
   handlePayerChange = (e) => {
-    if(this.state.checked == false){
+    if (this.state.checked == false) {
       this.handleCheck()
     }
     let members = this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup)
@@ -44,7 +44,7 @@ class AddTransaction extends React.Component {
     this.setState({
       selectedPayer: selectedMem
     })
-  
+
   }
 
   updateDetails = (e) => {
@@ -54,7 +54,7 @@ class AddTransaction extends React.Component {
         [e.target.name]: e.target.value,
         group_id: this.props.activeGroup,
       },
-      
+
 
     })
   }
@@ -88,7 +88,7 @@ class AddTransaction extends React.Component {
   }
 
   deleteMember = (e) => {
-    if(this.state.selectedPayer[0].member_name == e.target.name){
+    if (this.state.selectedPayer[0].member_name == e.target.name) {
       console.log(this.state.selectedPayer)
       this.handleCheck()
       return this.setState({
@@ -98,16 +98,16 @@ class AddTransaction extends React.Component {
     } else {
       e.preventDefault()
       this.setState({
-      group_members: this.state.group_members.filter(member => member.member_name !== e.target.name)
-    })
+        group_members: this.state.group_members.filter(member => member.member_name !== e.target.name)
+      })
     }
-    
+
   }
 
 
   render() {
-   let members = this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup) 
-   let splitMembers = members.filter(member => !this.state.group_members.includes(member))
+    let members = this.props.groupMembers.filter(({ group_id }) => group_id == this.props.activeGroup)
+    let splitMembers = members.filter(member => !this.state.group_members.includes(member))
     return (
       <>
         <div className="form-content">
@@ -115,45 +115,46 @@ class AddTransaction extends React.Component {
           {this.state.showTransactionForm &&
             <div className="animated fadeIn">
               <form className="transactionForm" onSubmit={this.submit}>
-              <div className="row">
-              <div className="col-lg-4 col-sm-12">
+                <div className="row">
+                  <div className="col-lg-4 col-sm-12">
 
-                <label className="inputLabel" >Description</label>
-                <input className='form-control' type='text' name='transactionName' placeholder="eg. Breakfast at Tiffany's" onChange={this.updateDetails}></input>
-                      </div>
+                    <label className="inputLabel" >Description</label>
+                    <input className='form-control' type='text' name='transactionName' placeholder="eg. Breakfast at Tiffany's" onChange={this.updateDetails}></input>
+                  </div>
                   <div className="col-lg-3 col-sm-12">
-                <label className="inputLabel">Paid by</label>
-                <select className='form-control' name='groupMemberId' onChange={this.updateDetails} onClick={this.handlePayerChange}>
-                <option>Select Member</option>
-                  {members.map((member, i) => {
-                     
-                    return <option value={member.groupMember_id} key={i}>{member.member_name}</option>
-                  })}
-                </select>
-                </div>
-                <div className="col-lg-2 col-sm-12 transactionAmountWrapper">
+                    <label className="inputLabel">Paid by</label>
+                    <select className='form-control' name='groupMemberId' onChange={this.updateDetails} onClick={this.handlePayerChange}>
+                      <option>Select Member</option>
+                      {members.map((member, i) => {
 
-                <label className="inputLabel">Amount $</label>
-                <input className='form-control' type='number' name='transactionTotal' placeholder='0.00' onChange={this.updateDetails} ></input>
-                </div>
+                        return <option value={member.groupMember_id} key={i}>{member.member_name}</option>
+                      })}
+                    </select>
+                  </div>
+                  <div className="col-lg-2 col-sm-12 transactionAmountWrapper">
+
+                    <label className="inputLabel">Amount $</label>
+                    <input className='form-control' type='number' name='transactionTotal' placeholder='0.00' onChange={this.updateDetails} ></input>
+                  </div>
 
                 </div>
                 <div>
                   <label className="inputLabel">Split by all members?</label>
                   <input type='checkbox' name='membersOwing' checked={this.state.checked} onChange={this.handleCheck}></input>
                   {!this.state.checked &&
-                  <>
-                    <select onChange={this.handlePayees} >
-                    <option>Select Member</option>
-                      {splitMembers.map((member, i) => {
-                        return <option key={member.member_name} value={member.member_name}>{member.member_name}</option>
+                    <>
+                      <div className="col-lg-3 col-sm-12">
+                        <select className="form-control" onChange={this.handlePayees} >
+                          <option>Select Member</option>
+                          {splitMembers.map((member, i) => {
+                            return <option key={member.member_name} value={member.member_name}>{member.member_name}</option>
+                          })}
+                        </select>
+                      </div>
+                      {this.state.group_members.map((member, i) => {
+                        return <ul className="formMembersList"> <li key={i + 1000} className="transactionFormMembersListItem">{member.member_name} <button className="btn btn-outline-danger btn-sm" name={member.member_name} onClick={this.deleteMember}>Remove</button></li></ul>
                       })}
-                    </select>
-                
-                    {this.state.group_members.map((member, i) => {
-                     return <ul> <li key={i+1000} className="formMembersListItem">{member.member_name} <button className="btn btn-outline-danger btn-sm" name={member.member_name} onClick={this.deleteMember}>Remove</button></li></ul>
-                    })}
-                  </>
+                    </>
                   }
                 </div>
                 <div>
